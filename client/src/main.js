51a59daea9053c1cff05e58636747c50fb51f707
josh@centosvm <Josh@JoshHaines.com>
2019-08-09 16:13:26 -0400

    added bootstrapvue

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
--- server/app.py
+++ server/app.py
@@ -1,6 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+# Book storage object
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 
 # configuration
 DEBUG = True
@@ -19,5 +38,14 @@ def ping_pong():
     return jsonify('pong!')
 
 
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+
+
+# caller
 if __name__ == '__main__':
     app.run()
