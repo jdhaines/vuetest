d4a15e6974da2bbde01fc0da18a673be636ba485
josh@centosvm <Josh@JoshHaines.com>
2019-08-09 15:57:04 -0400

    building books component

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,5 +1,35 @@
 <template>
     <div class="container">
-        <p>books</p>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1>Books</h1>
+                <hr><br/><br/>
+                <button type="button" class="btn btn-success btn-sm">Add Book</button>
+                <br/><br/>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Read?</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Foo</td>
+                            <td>Bar</td>
+                            <td>Foobar</td>
+                            <td>
+                                <div class="btn-toolbar" role="group">
+                                    <button type="button" class="btn btn-warning btn-sm">Update</button>&nbsp
+                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </template>
